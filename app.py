79c198feb7867a458b26f0a80983f38467c896e9
marcoscandeira62-sudo <marcoscandeira62@gmail.com>
2026-01-20 09:17:36 +0100

    Initialize Flask app with SocketIO support

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, send
+
+app = Flask(__name__)
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@socketio.on("message")
+def recibir_mensaje(msg):
+    send(msg, broadcast=True)
+
+if __name__ == "__main__":
+    socketio.run(app)
